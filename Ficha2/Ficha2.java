9b3ca799f750e2785e69e56d2f567c77a8e42da0
Samba.lnv <openwac.samba@gmail.com>
2023-02-28 10:18:32 +0000

    Exercicios da ficha 2, exercicio 1.3 minado

---
+++ Ficha2/Ficha2.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class Ficha2
+{
+    private boolean exists (int a[], int x)
+    {
+        for (int i= 0; i< a.length; i++)
+        {
+            if (a[i]== x)
+                return true;
+        }
+        return false;
+    }
+    private int[] push (int a[], int pos, int x)
+    {
+        if (pos== a.length)
+        {
+            int[] r= Arrays.copyOf (a, a.length* 2);
+            a= r;
+        }
+        a[pos]= x;
+        return a;
+    }
+    //API
+    public int max (int[] a, int N)
+    {
+        if (N>0)
+        {
+            int max= a[0];
+            for (int i= 1; i< N; i++)
+            {
+                max= (max> a[i]) ? max : a[i];
+            }
+            return max;
+        }
+        else
+            return 0;
+    }
+    public int[] byindex (int x, int y, int[] a)
+    {
+        int N= y- x;
+        if (N> 0)
+        {
+            int r[]= new int[N];
+            for (int i= 0; i< N; i++)
+            {
+                r[i]= a[x+ i];
+            }
+            return r;
+        }
+        else
+            return null;
+    }
+    public int[] cross (int a[], int N, int b[], int Y)
+    {
+        if (N< Y)
+        {
+            int pos= 0;
+            int r[]= new int[10];
+            for (int i=0 ; i< N; i++)
+            {
+                if (exists (b, a[i]))
+                    r= push (r, pos++, a[i]); 
+            }
+            if (pos!= r.length)
+            {
+                int temp[]= Arrays.copyOf(r, pos);
+                r= temp;
+            }
+            return r;
+        }
+        else
+            return null;
+    }
+}
---
+++ Ficha2/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.lang.Integer;
+
+public class Main 
+{
+    private static String ex1 (Ficha2 ficha, Scanner input)
+    {
+        System.out.println ("Introduza tamanho do array:");
+        int n= input.nextInt ();
+        int[] a= new int[n];
+        for (int i= 0; i< n; i++)
+            a[i]= input.nextInt();
+        return "O valor máximo do array é: " + Integer.toString(ficha.max (a, n));
+    }
+    private static String ex2 (Ficha2 ficha, Scanner input)
+    {
+        System.out.println ("Introduza o tamanho do array:");
+        int n= input.nextInt();
+        int[] a= new int[n];
+        System.out.println ("Introduza o intervalo que deseja:");
+        int x= input.nextInt();
+        int y= input.nextInt();
+        System.out.println("Introduza os membros do array:");
+        for (int i= 0; i< n; i++)
+            a[i]= input.nextInt();
+        return "Os valores do índex são: "+ Arrays.toString (ficha.byindex (x, y, a));
+    }
+    private static String ex3 (Ficha2 ficha, Scanner input)
+    {
+        System.out.println ("Introduza o tamanho dos arrays:");
+        int N= input.nextInt();
+        int a[]= new int[N];
+        int Y= input.nextInt();
+        int b[]= new int[Y];
+        System.out.println ("Introduza o 1º array:");
+        for (int i= 0; i< N; i++)
+            a[i]= input.nextInt();
+        System.out.println ("Introduza o 2º array:");
+        for (int i= 0; i< Y; i++)
+            b[i]= input.nextInt();
+        return "Os valores em comum são: "+ Arrays.toString(ficha.cross(a, N, b, Y));
+    }
+    
+    public static void main (String[] args)
+    {
+        if (args.length== 0)
+        {
+            System.out.println ("No args? *insert MegaMind meme*");
+            System.exit(0);
+        }
+        int n= Integer.parseInt (args[0]);                                      //Num Exercicio
+        Scanner input= new Scanner (System.in);
+        Ficha2 ficha= new Ficha2 ();
+        String s= "";
+        switch (n)
+        {
+            case 1:
+                s= ex1 (ficha, input);
+                break;
+            case 2:
+                s= ex2 (ficha, input);
+                break;
+            case 3:
+                s= ex3 (ficha, input);
+                break;
+            default:
+                break;
+        }
+        System.out.println (s);
+        input.close ();
+    }
+}
